0da4b19397c6ae1d44040f7785486d59969e788d
radnaev476 <78133730+radnaev476@users.noreply.github.com>
2021-01-28 14:42:33 +0800

    Add files via upload

---
+++ run-app-as-non-admin.bat
@@ -0,0 +1,2 @@
+Set ApplicationPath="C:\Steam\Steam.exe"
+cmd / min /c "set __COMPAT_LAYER= RunAsInvoker && start "" %ApplicationPath%"
---
+++ sdsdad.py
@@ -0,0 +1,16 @@
+def vichislenie(a,b,c):
+    a = int(input())
+    b = int(input())
+    c = input()
+    if c = +:
+        return a + b
+    elif c == -:
+        return a - b
+    elif c == *:
+        return a * b
+    elif c == /:
+        return a / b
+    else 
+        return "idi nafig"
+        
+print(vichislenie)
